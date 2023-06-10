5aa55f311a5a9b2bc51d01aa2716cb022961b3f5
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-06-10 15:00:58 +0300

    updated failing test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,10 @@ RSpec.describe User, type: :model do
   describe '#recent_posts' do
     let(:new_user) { User.create(name: 'Kunda', photo: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d', bio: 'Tella') }
 
+    it 'returns zero(0) when the user is created' do
+      expect(new_user.recent_posts.count).to eq 0
+    end
+
     before do
       Post.create(title: 'Test Post', author: new_user, text: 'This is a test post', comment_counter: 0,
                   likes_counter: 0)
@@ -34,10 +38,6 @@ RSpec.describe User, type: :model do
       Post.create(author: new_user, title: 'Lugha', text: 'Unasema Lugha?', comment_counter: 0, likes_counter: 0)
       Post.create(author: new_user, title: 'Peace Maker', text: 'I come in peace?', comment_counter: 0,
                   likes_counter: 0)
-    end
-
-    it 'returns zero(0) when the user is created' do
-      expect(new_user.recent_posts.count).to eq 0
     end
 
     it 'returns the last 3 posts' do
